ac9a065d7adc91698008e25574fae7c64900f9ad
Sabina Bakun <bakun.s@mysales-labs.com>
2018-12-24 13:54:03 +0200

    Edited App form

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import Film from './../Film';
+import FilmList from './../FilmList';
+import FilmEdit from './../FilmEdit';
+import { films } from './../../data.js';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <header className='App-header'>
-          <Film />
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            films: films,
+            editFilmId: null
+        }
+    }
+
+    onClickEdit = id => () => this.setState({ editFilmId: id });
+
+    onClickCancel = () => this.setState({ editFilmId: null });
+    
+    onClickSave = film => () => {
+        const { films, editFilmId } = this.state;
+
+        const newFilms = [ ...films ];
+        const filmIndex = films.findIndex(film => film.id === editFilmId);
+        
+        film.year = +film.year;
+        newFilms[filmIndex] = { ...film };
+
+        this.setState({ films: newFilms, editFilmId: null });
+    }
+
+    render() {
+        const { films, editFilmId } = this.state;
+        const film = films.find(film => film.id === editFilmId);
+
+        return (
+            <div className='App'>
+                <header className='App-header'>
+                    { 
+                        film ? 
+                        <FilmEdit film={film} onClickSave={this.onClickSave} onClickCancel={this.onClickCancel} /> : 
+                        <FilmList films={films} onClickEdit={this.onClickEdit} /> 
+                    }
+                </header>
+            </div>
+        );
+    }
 }
+
+App.propTypes = { 
+    films: PropTypes.array, 
+    editFilmId: PropTypes.string
+};
 
 export default App;
