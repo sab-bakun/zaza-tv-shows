8f6672d8d2d547c4f51e457ed2b67459530f6674
Sabina Bakun <bakun.s@mysales-labs.com>
2018-12-24 13:53:37 +0200

    Edited Film form

--- src/components/Film/index.js
+++ src/components/Film/index.js
@@ -1,43 +1,43 @@
-import React, { Component } from 'react';
-import {Card, CardHeader, CardMedia, CardContent, Typography} from '@material-ui/core';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, CardHeader, CardMedia, CardContent, Typography, IconButton } from '@material-ui/core';
+import EditIcon from '@material-ui/icons/Edit';
 
 import './Film.css';
 
-class Film extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: `Breakfast at Tiffany's`,
-            year: '1961',
-            country: 'USA',
-            description: `Holly Golightly is a superficial, ambitious, 
-                money-orientated New York socialite. Paul Varjak, a writer, 
-                moves into her building and they become friends. Ultimately 
-                Paul falls in love with Holly but will she reciprocate or follow her ambitions?`,
-            banner: 'images/banner.jpg'
-        };
-    }
+const Film = ({ film, onClickEdit }) => 
+    <Card className='Film'>
+        <CardHeader
+            title={film.name}
+            subheader={film.year + ' - ' + film.country}
+            action={
+                <IconButton onClick={onClickEdit(film.id)}>
+                    <EditIcon />
+                </IconButton>
+            }
+        />
+        <CardMedia
+            className='Film-banner'
+            image={film.banner}
+            title={film.name}
+        />
+        <CardContent>
+            <Typography className='Film-description'>
+                {film.description}
+            </Typography>
+        </CardContent>
+    </Card>;
 
-    render() {
-        return (
-            <Card className='Film'>
-                <CardHeader
-                    title={this.state.name}
-                    subheader={this.state.year + ' - ' + this.state.country}
-                />
-                <CardMedia
-                    className='Film-banner'
-                    image='images/banner.jpg'
-                    title={this.state.name}
-                />
-                <CardContent>
-                    <Typography>
-                        {this.state.description}
-                    </Typography>
-                </CardContent>
-            </Card>
-        );
-    }
-}
+Film.propTypes = { 
+    film: PropTypes.shape({
+        id: PropTypes.string,
+        name: PropTypes.string,
+        year: PropTypes.number,
+        country: PropTypes.string,
+        description: PropTypes.string,
+        banner: PropTypes.string
+    }),
+    onClickEdit: PropTypes.func
+};
 
 export default Film;
