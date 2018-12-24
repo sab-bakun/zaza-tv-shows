063db77523140331bc38b1dae6ba51ae33a2665b
Sabina Bakun <bakun.s@mysales-labs.com>
2018-12-24 13:51:52 +0200

    Added films data

---
+++ src/data.js
@@ -0,0 +1,82 @@
+export const films = [
+    {
+        "id": "001",
+        "name": "Breakfast at Tiffany's",
+        "year": 1961,
+        "country": "USA",
+        "description": "Holly Golightly is a superficial, ambitious, money-orientated New York socialite. Paul Varjak, a writer, moves into her building and they become friends. Ultimately Paul falls in love with Holly but will she reciprocate or follow her ambitions?",
+        "banner": "images/banner_001.jpg"
+    },
+    {
+        "id": "002",
+        "name": "The Shawshank Redemption",
+        "year": 1994,
+        "country": "USA",
+        "description": "In 1947, Andy Dufresne (Tim Robbins), a banker in Maine, is convicted of murdering his wife and her lover, a golf pro. Since the state of Maine has no death penalty, he is given two consecutive life sentences and sent to the notoriously harsh Shawshank Prison.",
+        "banner": "images/banner_002.jpg"
+    },
+    {
+        "id": "003",
+        "name": "Hachi: A Dog's Tale",
+        "year": 2009,
+        "country": "USA",
+        "description": "Professor Parker Wilson takes an abandoned dog at the train station home with the intention of returning the animal to its owner. He finds the dog is an Akita and names it Hachiko. Nobody claims the dog, so his family decides to keep Hachi.",
+        "banner": "images/banner_003.jpg"
+    },
+    {
+        "id": "004",
+        "name": "Leon",
+        "year": 1994,
+        "country": "France",
+        "description": "Mathilda, a 12-year-old girl, is reluctantly taken in by Léon, a professional assassin, after her family is murdered. Léon and Mathilda form an unusual relationship, as she becomes his protégée and learns the assassin's trade.",
+        "banner": "images/banner_004.jpg"
+    },
+    {
+        "id": "005",
+        "name": "Seven Pounds",
+        "year": 2008,
+        "country": "USA",
+        "description": "A man with a fateful secret embarks on an extraordinary journey of redemption by forever changing the lives of seven strangers. Haunted by a secret, Ben Thomas looks for redemption by radically transforming the lives of seven people he doesn't know. Once his plan is set, nothing will be able to stop him.",
+        "banner": "images/banner_005.jpg"
+    },
+    {
+        "id": "006",
+        "name": "Knockin' on Heaven's Door",
+        "year": 1997,
+        "country": "Germany",
+        "description": "Two young men, Martin and Rudi, both suffering from terminal cancer, get to know each other in a hospital room. They drown their desperation in Tequila and decide to take one last trip to the sea. Drunk and still in pajamas they steal the first fancy car they find, a 60's Mercedes convertible. The car happens to belong to a bunch of gangsters, which immediately start to chase it, since it contains more than the pistol Martin finds in the glove box.",
+        "banner": "images/banner_006.jpg"
+    },
+    {
+        "id": "007",
+        "name": "The Bucket List",
+        "year": 2007,
+        "country": "USA",
+        "description": "Edward meets Carter, when they in hospital. They both live different lifestyles, but become friends when they bond because of their common terminal illnesses. They later come together to make a list of things they want to do before they: \"kick the bucket\".",
+        "banner": "images/banner_007.jpg"
+    },
+    {
+        "id": "008",
+        "name": "The Pursuit of Happyness",
+        "year": 2006,
+        "country": "USA",
+        "description": "A struggling salesman takes custody of his son as he's poised to begin a life-changing professional career. Based on a true story about a man named Christopher Gardner. Gardner has invested heavily in a device known as a \"bone density scanner\". He feels like he has it made selling these devices.",
+        "banner": "images/banner_008.jpg"
+    },
+    {
+        "id": "009",
+        "name": "The Devil's Advocate",
+        "year": 1997,
+        "country": "USA",
+        "description": "An exceptionally adept Florida lawyer is offered a job at a high-end New York City law firm with a high-end boss - the biggest opportunity of his career to date.",
+        "banner": "images/banner_009.jpg"
+    },
+    {
+        "id": "010",
+        "name": "Me Before You",
+        "year": 2016,
+        "country": "USA",
+        "description": "Louisa Clark is a small town girl who knows a lot about the people around her. Will is a quadriplegic man who hates everyone around him. Louisa is hired by Will's mum to take care of him for six months. Although they have a rocky start, their relationship grows over time, going for hate, to like, to love.",
+        "banner": "images/banner_010.jpg"
+    }
+];
